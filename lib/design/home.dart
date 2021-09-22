0612edf370699e39155551c860ef0138dee59aa5
yu07032 <u07032@daum.net>
2021-09-22 17:34:14 +0900

    + 구글맵 연동, 메인화면 구글맵, 정해진 좌표 이동

--- lib/design/home.dart
+++ lib/design/home.dart
@@ -1,8 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_designtest01/design/testpage1.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class home extends StatelessWidget {
-  const home({Key? key}) : super(key: key);
+class home extends StatefulWidget {
+  @override
+  State<home> createState() => homeState();
+
+}
+class homeState extends State<home>{
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      //bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      //tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +82,22 @@ class home extends StatelessWidget {
         ),
 
         //홈 구글맵 구현
-        body: Text('구글맵 구현'),
+        body: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller){
+           _controller.complete(controller);
+          },
+        ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: _goToMap,
+          label: Text('이동')
+      ),
       );
+  }
+  
+  Future<void> _goToMap() async{
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
